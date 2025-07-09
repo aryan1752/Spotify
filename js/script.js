e6612598c37235ed296d8951d85eaabe7d879fcf
aryan1752 <jharozy24@gmail.com>
2025-07-09 10:53:26 +0530

    update

--- js/script.js
+++ js/script.js
@@ -1,38 +1,56 @@
-async function getSongs() {
-    // Test if the connection is working by fetching from the root
-    fetch("http://127.0.0.1:5500/")
-        .then(response => response.text())  // Get the response text
-        .then(data => console.log("Server response:", data))  // Log the response to check if the connection works
-        .catch(err => console.error("Error fetching:", err));  // Log any errors
+const playMusic = (track) => {
+    let audio = new Audio("/songs/" + track);
+    audio.play();
+};
 
-    // Continue with the original fetch for songs
+async function getSongs() {
     let a = await fetch("http://127.0.0.1:5500/songs/");
     let response = await a.text();
-
     let div = document.createElement("div");
     div.innerHTML = response;
-
     let as = div.getElementsByTagName("a");
     let songs = [];
 
     for (let index = 0; index < as.length; index++) {
         const element = as[index];
         if (element.href.endsWith(".mp3")) {
-            songs.push(element.href);
+            songs.push(decodeURIComponent(element.href.split("/songs/")[1]));
         }
     }
+
     return songs;
-}
+};
 
 async function main() {
-    // Call the getSongs function to retrieve the song links
     let songs = await getSongs();
-
-    // Log the songs to the console
     console.log(songs);
-    var audio = new Audio(songs[0]);
-    audio.play();
+
+    let songUL = document.querySelector(".songList ul");
+    songUL.innerHTML = ""; // Clear the existing list
+
+    songs.forEach(song => {
+        let li = document.createElement("li");
+
+        li.innerHTML = `
+            <img class="invert small-icon" src="img/music.svg" alt="">
+            <div class="info">
+                <div>${song.replaceAll("%20", " ").replaceAll("%40", "@")}</div>
+                <div>Unknown Artist</div>
+            </div>
+            <img src="img/play.svg" alt="play" class="play-group">
+        `;
+
+        // Click event for playing the song
+        li.addEventListener("click", () => {
+            console.log("Playing:", song);
+            playMusic(song);
+        });
+
+        songUL.appendChild(li);
+    });
+
+    // Optional: auto-play first song
+    // playMusic(songs[0]);
 }
 
-// Execute the main function
-main();   
+main();
